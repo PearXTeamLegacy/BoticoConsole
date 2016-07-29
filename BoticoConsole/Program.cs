6ba97a6e719ffc0e9978934ff34789591f76559c
mrAppleXZ <mr.applexz@gmail.com>
2016-07-29 22:15:53 +0500

    Init

---
+++ BoticoConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Botico;
+
+namespace BoticoConsole
+{
+	class MainClass
+	{
+		public static BoticoClient Botico = new BoticoClient('/', "Console", false);
+
+		public static void Main(string[] args)
+		{
+			Botico.Init();
+			ReadLine();
+		}
+
+		public static void ReadLine()
+		{
+			string s = Console.ReadLine();
+			string response = Botico.UseCommand(s, Environment.UserName, false);
+			if (!string.IsNullOrEmpty(response))
+				Console.WriteLine(response);
+			else
+				Console.WriteLine("Unknown command!");
+			ReadLine();
+		}
+	}
+}
